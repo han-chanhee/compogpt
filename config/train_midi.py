3ca1fea5dd917fa472eed11827cd1f50863f1e6c
Han_code <93130655+han-chanhee@users.noreply.github.com>
2023-08-05 15:51:43 +0900

    Update train_midi.py

--- config/train_midi.py
+++ config/train_midi.py
@@ -15,7 +15,7 @@ wandb_run_name = "mini-gpt"
 
 dataset = "midi2text_demo"
 gradient_accumulation_steps = 1
-batch_size = 64
+batch_size = 32
 block_size = 512  # context of up to 256 previous characters
 
 # baby GPT model :)
