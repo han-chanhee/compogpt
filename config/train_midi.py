a19af8d262546251ad61812991e8cb7924179d1b
Han_code <93130655+han-chanhee@users.noreply.github.com>
2023-08-07 22:00:54 +0900

    Update train_midi.py

--- config/train_midi.py
+++ config/train_midi.py
@@ -2,8 +2,8 @@
 # good for debugging and playing on macbooks and such
 
 out_dir = "out-midi"
-eval_interval = 500  # keep frequent because we'll overfit
-eval_iters = 200
+eval_interval = 1000  # keep frequent because we'll overfit
+eval_iters = 400
 log_interval = 100  # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
@@ -15,17 +15,17 @@ wandb_run_name = "mini-gpt"
 
 dataset = "giant_MIDI_midi2text"
 gradient_accumulation_steps = 1
-batch_size = 64
+batch_size = 32
 block_size = 512  # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 8
-n_head = 8
-n_embd = 768
+n_layer = 48
+n_head = 16
+n_embd = 1024
 dropout = 0.2
 
 learning_rate = 1e-3  # with baby networks can afford to go a bit higher
-max_iters = 10000
+max_iters = 100000
 lr_decay_iters = 10000  # make equal to max_iters usually
 min_lr = 1e-4  # learning_rate / 10 usually
 beta2 = 0.99  # make a bit bigger because number of tokens per iter is small
