9f11e5a21300402244d3d4920fc73f5884e3a30a
Han_code <93130655+han-chanhee@users.noreply.github.com>
2023-08-08 03:53:43 +0900

    Update train_midi.py

--- config/train_midi.py
+++ config/train_midi.py
@@ -21,7 +21,7 @@ block_size = 512  # context of up to 256 previous characters
 # baby GPT model :)
 n_layer = 12
 n_head = 12
-n_embd = 432
+n_embd = 768
 dropout = 0.2
 
 learning_rate = 1e-3  # with baby networks can afford to go a bit higher
