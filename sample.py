e81367a2ac3e051b9250d08b6727eb2e4ae0e981
Han_code <93130655+han-chanhee@users.noreply.github.com>
2023-08-07 21:04:23 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -15,7 +15,7 @@ init_from = (
 out_dir = "out"  # ignored if init_from is not 'resume'
 start = "\n"  # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
 num_samples = 10  # number of samples to draw
-max_new_tokens = 10000  # number of tokens generated in each sample
+max_new_tokens = 5000  # number of tokens generated in each sample
 temperature = (
     0.8  # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 )
