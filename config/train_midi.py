c32e3aa17d9c4f036fdce58127536d1986df3cf2
Han_code <93130655+han-chanhee@users.noreply.github.com>
2023-08-08 13:13:44 +0900

    Update train_midi.py

--- config/train_midi.py
+++ config/train_midi.py
@@ -4,7 +4,7 @@
 out_dir = "out-midi"
 eval_interval = 1000  # keep frequent because we'll overfit
 eval_iters = 400
-log_interval = 500  # don't print too too often
+log_interval = 100  # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
 always_save_checkpoint = False
@@ -25,8 +25,8 @@ n_embd = 768
 dropout = 0.2
 
 learning_rate = 1e-3  # with baby networks can afford to go a bit higher
-max_iters = 50000
-lr_decay_iters = 50000  # make equal to max_iters usually
+max_iters = 70000
+lr_decay_iters = 70000  # make equal to max_iters usually
 min_lr = 1e-4  # learning_rate / 10 usually
 beta2 = 0.99  # make a bit bigger because number of tokens per iter is small
 
