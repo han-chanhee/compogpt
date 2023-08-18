c58ce4846da1e76784c541e539a5a0cedb1906f6
Han_code <93130655+han-chanhee@users.noreply.github.com>
2023-08-18 21:39:27 +0900

    Create prepare_sp.py
    새로운 토큰화 방식

---
+++ data/giant_MIDI_midi2text/prepare_sp.py
@@ -0,0 +1,32 @@
+import os
+import sentencepiece as spm
+import numpy as np
+
+# Load or train SentencePiece model
+spm.SentencePieceTrainer.train(input='giant_midi.txt', model_prefix='spm_model', vocab_size=1000)
+sp = spm.SentencePieceProcessor()
+sp.load('spm_model.model')
+
+# Load data
+with open("giant_midi.txt", "r") as f:
+    data = f.read()
+
+# Tokenize using SentencePiece
+train_tokens = sp.encode_as_pieces(data[: int(len(data) * 0.9)])
+val_tokens = sp.encode_as_pieces(data[int(len(data) * 0.9) :])
+
+# Count unique tokens
+unique_train_tokens = len(set(train_tokens))
+unique_val_tokens = len(set(val_tokens))
+print("Unique Train Tokens:", unique_train_tokens)
+print("Unique Val Tokens:", unique_val_tokens)
+
+# Convert tokens to IDs
+train_ids = sp.encode_as_ids(data[: int(len(data) * 0.9)])
+val_ids = sp.encode_as_ids(data[int(len(data) * 0.9) :])
+
+# Export to binary files
+train_ids = np.array(train_ids, dtype=np.uint16)
+val_ids = np.array(val_ids, dtype=np.uint16)
+train_ids.tofile(os.path.join(os.path.dirname(__file__), "train.bin"))
+val_ids.tofile(os.path.join(os.path.dirname(__file__), "val.bin"))
