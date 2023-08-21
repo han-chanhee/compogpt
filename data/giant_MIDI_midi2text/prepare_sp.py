31f77fc4ef4f6c5c3fcd13fdfffa691e1eed9a6b
Han_code <93130655+han-chanhee@users.noreply.github.com>
2023-08-21 11:38:36 +0900

    Update prepare_sp.py

--- data/giant_MIDI_midi2text/prepare_sp.py
+++ data/giant_MIDI_midi2text/prepare_sp.py
@@ -20,13 +20,7 @@ with open("giant_midi2.txt", "r") as f:
 train_tokens = sp.encode_as_pieces(data[: int(len(data) * 0.9)])
 val_tokens = sp.encode_as_pieces(data[int(len(data) * 0.9) :])
 
-# Count unique tokens
-unique_train_tokens = len(set(train_tokens))
-unique_val_tokens = len(set(val_tokens))
-print(len(unique_train_tokens))
-print("Unique Train Tokens:", unique_train_tokens)
-print(len(unique_val_tokens))
-print("Unique Val Tokens:", unique_val_tokens)
+
 
 # Convert tokens to IDs
 train_ids = sp.encode_as_ids(data[: int(len(data) * 0.9)])
