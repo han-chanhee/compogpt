ff58853142d98c4c5ce06fa601f62a2bc0273071
Han_code <93130655+han-chanhee@users.noreply.github.com>
2023-10-28 19:41:02 +0900

    Update train.py

--- train.py
+++ train.py
@@ -29,7 +29,7 @@ from torch.distributed import init_process_group, destroy_process_group
 
 from model import GPTConfig, GPT
 import torch._dynamo
-    torch._dynamo.config.suppress_errors = True
+torch._dynamo.config.suppress_errors = True
 
 # -----------------------------------------------------------------------------
 # default config values designed to train a gpt2 (124M) on OpenWebText
