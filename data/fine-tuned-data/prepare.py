bf3b69a46433f80c4621c1bd377234ab22aec4f6
Han_code <93130655+han-chanhee@users.noreply.github.com>
2023-10-31 14:21:20 +0900

    Update prepare.py

--- data/fine-tuned-data/prepare.py
+++ data/fine-tuned-data/prepare.py
@@ -2,8 +2,14 @@ import os
 import tiktoken
 import numpy as np
 
+# 여러 개의 스페셜 토큰 추가
+special_tokens = ["<sos>", "<eos>"]
+for i in range(1,3305):
+    special_tokens+=f"[i]"
+enc = tiktoken.get_encoding("gpt2")
+special_token_ids = [enc.encode_ordinary(token)[0] for token in special_tokens]
 
-with open("combined_formatted_data.txt", "r") as f:
+with open("giant_midi2.txt", "r") as f:
     data = f.read()
 n = len(data)
 train_data = data[: int(n * 0.9)]
@@ -13,6 +19,11 @@ val_data = data[int(n * 0.9) :]
 enc = tiktoken.get_encoding("gpt2")
 train_ids = enc.encode_ordinary(train_data)
 val_ids = enc.encode_ordinary(val_data)
+
+# 여러 개의 스페셜 토큰 추가
+train_ids.extend(special_token_ids)
+val_ids.extend(special_token_ids)
+
 print(len(set(train_ids)))
 print(set(train_ids))
 print(len(set(val_ids)))
@@ -26,5 +37,5 @@ val_ids = np.array(val_ids, dtype=np.uint16)
 train_ids.tofile(os.path.join(os.path.dirname(__file__), "train.bin"))
 val_ids.tofile(os.path.join(os.path.dirname(__file__), "val.bin"))
 
-# train.bin has 301,966 tokens
-# val.bin has 36,059 tokens
+# train.bin has 301,970 tokens
+# val.bin has 36,063 tokens
