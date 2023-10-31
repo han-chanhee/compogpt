336cef845a9ea93b3f818b630ce9b790f5befb27
Han_code <93130655+han-chanhee@users.noreply.github.com>
2023-10-31 17:36:37 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -13,7 +13,7 @@ init_from = (
     "resume"  # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
 )
 out_dir = "out"  # ignored if init_from is not 'resume'
-start = "\n"  # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
+start = "<sos>"  # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
 num_samples = 10  # number of samples to draw
 max_new_tokens = 5000  # number of tokens generated in each sample
 temperature = (
@@ -72,7 +72,7 @@ if compile:
     model = torch.compile(model)  # requires PyTorch 2.0 (optional)
 
 # look for the meta pickle in case it is available in the dataset folder
-load_meta = False
+load_meta = True
 if (
     init_from == "resume"
     and "config" in checkpoint
