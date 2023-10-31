24b4e1013c2425cd2eca50cd2ddf0694d01a7702
Han_code <93130655+han-chanhee@users.noreply.github.com>
2023-10-31 12:19:26 +0900

    Update finetune_midi.py

--- config/finetune_midi.py
+++ config/finetune_midi.py
@@ -1,25 +1,42 @@
 import time
 
-out_dir = 'out-shakespeare'
-eval_interval = 5
-eval_iters = 40
-wandb_log = False # feel free to turn on
-wandb_project = 'shakespeare'
-wandb_run_name = 'ft-' + str(time.time())
 
-dataset = 'shakespeare'
-init_from = 'gpt2-xl' # this is the largest GPT-2 model
+init_from = 'resume' # this is the largest GPT-2 model
 
-# only save checkpoints if the validation loss improves
+# train a miniature character-level shakespeare model
+# good for debugging and playing on macbooks and such
+
+out_dir = "out-midi"
+eval_interval = 1000  # keep frequent because we'll overfit
+eval_iters = 400
+log_interval = 100  # don't print too too often
+
+# we expect to overfit on this small dataset, so only save when val improves
 always_save_checkpoint = False
 
-# the number of examples per iter:
-# 1 batch_size * 32 grad_accum * 1024 tokens = 32,768 tokens/iter
-# shakespeare has 301,966 tokens, so 1 epoch ~= 9.2 iters
-batch_size = 1
-gradient_accumulation_steps = 32
-max_iters = 20
+wandb_log = False  # override via command line if you like
+wandb_project = "fine-tuned-data"
+wandb_run_name = "fine-tuned-compoGPT"
 
-# finetune at constant LR
-learning_rate = 3e-5
-decay_lr = False
+dataset = "fine-tuned-data"
+gradient_accumulation_steps = 2
+batch_size = 32
+block_size = 256  # context of up to 256 previous characters
+
+# baby GPT model :)
+n_layer = 12
+n_head = 12
+n_embd = 768
+dropout = 0.2
+
+learning_rate = 1e-3  # with baby networks can afford to go a bit higher
+max_iters = 30000
+lr_decay_iters = 70000  # make equal to max_iters usually
+min_lr = 1e-4  # learning_rate / 10 usually
+beta2 = 0.99  # make a bit bigger because number of tokens per iter is small
+
+warmup_iters = 100  # not super necessary potentially
+
+# on macbook also add
+# device = 'cpu'  # run on cpu only
+# compile = False # do not torch compile the model
