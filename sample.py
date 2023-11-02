e6be1ce631e5910a7dff9db84a9c7547cdb0de57
Han_code <93130655+han-chanhee@users.noreply.github.com>
2023-11-02 15:00:40 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,29 +1,43 @@
+"""
+Sample from a trained model
+"""
 import os
 import pickle
+from contextlib import nullcontext
 import torch
 import tiktoken
 from model import GPTConfig, GPT
-from contextlib import nullcontext
 
 # -----------------------------------------------------------------------------
-init_from = "resume"
-out_dir = "out-midi"
-start = "FILE:prompt.txt"
-num_samples = 10
-max_total_tokens = 200
-temperature = 0.8
-top_k = 200
+init_from = (
+    "resume"  # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
+)
+out_dir = "out"  # ignored if init_from is not 'resume'
+start = "<SOS>"  # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
+num_samples = 10  # number of samples to draw
+max_new_tokens = 500  # number of tokens generated in each sample
+temperature = (
+    0.8  # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
+)
+top_k = (
+    200  # retain only the top_k most likely tokens, clamp others to have 0 probability
+)
 seed = 1337
-device = "cuda"
-dtype = "bfloat16" if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else "float16"
-compile = False
+device = "cuda"  # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
+dtype = (
+    "bfloat16"
+    if torch.cuda.is_available() and torch.cuda.is_bf16_supported()
+    else "float16"
+)  # 'float32' or 'bfloat16' or 'float16'
+compile = False  # use PyTorch 2.0 to compile the model to be faster
+exec(open("configurator.py").read())  # overrides from command line or config file
 # -----------------------------------------------------------------------------
 
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
-torch.backends.cuda.matmul.allow_tf32 = True
-torch.backends.cudnn.allow_tf32 = True
-device_type = "cuda" if "cuda" in device else "cpu"
+torch.backends.cuda.matmul.allow_tf32 = True  # allow tf32 on matmul
+torch.backends.cudnn.allow_tf32 = True  # allow tf32 on cudnn
+device_type = "cuda" if "cuda" in device else "cpu"  # for later use in torch.autocast
 ptdtype = {
     "float32": torch.float32,
     "bfloat16": torch.bfloat16,
@@ -35,8 +49,9 @@ ctx = (
     else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
 )
 
-# 모델 초기화
+# model
 if init_from == "resume":
+    # init from a model saved in a specific directory
     ckpt_path = os.path.join(out_dir, "ckpt.pt")
     checkpoint = torch.load(ckpt_path, map_location=device)
     gptconf = GPTConfig(**checkpoint["model_args"])
@@ -45,45 +60,77 @@ if init_from == "resume":
     unwanted_prefix = "_orig_mod."
     for k, v in list(state_dict.items()):
         if k.startswith(unwanted_prefix):
-            state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
+            state_dict[k[len(unwanted_prefix) :]] = state_dict.pop(k)
     model.load_state_dict(state_dict)
 elif init_from.startswith("gpt2"):
+    # init from a given GPT-2 model
     model = GPT.from_pretrained(init_from, dict(dropout=0.0))
 
 model.eval()
 model.to(device)
 if compile:
-    model = torch.compile(model)
+    model = torch.compile(model)  # requires PyTorch 2.0 (optional)
 
-# 파일에서 시작 텍스트 읽어오기
+# look for the meta pickle in case it is available in the dataset folder
+load_meta = False
+if (
+    init_from == "resume"
+    and "config" in checkpoint
+    and "dataset" in checkpoint["config"]
+):  # older checkpoints might not have these...
+    meta_path = os.path.join("data", checkpoint["config"]["dataset"], "meta.pkl")
+    load_meta = os.path.exists(meta_path)
+if load_meta:
+    print(f"Loading meta from {meta_path}...")
+    with open(meta_path, "rb") as f:
+        meta = pickle.load(f)
+    # TODO want to make this more general to arbitrary encoder/decoder schemes
+    stoi, itos = meta["stoi"], meta["itos"]
+    encode = lambda s: [stoi[c] for c in s]
+    decode = lambda l: "".join([itos[i] for i in l])
+else:
+    # ok let's assume gpt-2 encodings by default
+    print("No meta.pkl found, assuming GPT-2 encodings...")
+    enc = tiktoken.get_encoding("gpt2")
+    encode = lambda s: enc.encode(s, allowed_special={"<EOS>"})
+    decode = lambda l: enc.decode(l)
+
+# encode the beginning of the prompt
 if start.startswith("FILE:"):
     with open(start[5:], "r", encoding="utf-8") as f:
-        start_text = f.read()
-else:
-    start_text = start
-
-# 시작 텍스트를 인코딩하여 텐서로 변환
-enc = tiktoken.get_encoding("gpt2")
-encode = lambda s: enc.encode(s, allowed_special={""})
-start_ids = encode(start_text)
+        start = f.read()
+start_ids = encode(start)
 x = torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...]
 
-# 생성된 샘플 저장 폴더 생성
-output_dir = out_dir
-os.makedirs(output_dir, exist_ok=True)
-
-# 샘플 생성 및 저장
+# run generation
+sample_count = 1
+total_lines = 0
 with torch.no_grad():
     with ctx:
-        for k in range(num_samples):
-            generated_tokens = []
-            while len(generated_tokens) < max_total_tokens:
-                y = model.generate(x, max_total_tokens, temperature=temperature, top_k=top_k)
-                generated_tokens.extend(y[0].tolist())
-                if "[" in enc.decode([y[0][-1]]):
-                    break
+        while total_lines < 200:  # 텍스트가 200줄을 넘을 때까지 반복
+            y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
+            generated_text = decode(y[0].tolist())
 
-            generated_text = enc.decode(generated_tokens)
-            output_path = os.path.join(output_dir, f"sample_{k+1}.txt")
+            # "[숫자]"를 텍스트에 삽입
+            generated_text = generated_text.replace("[", f"[{sample_count}]")
+
+            # 생성된 텍스트에 "["가 없을 때까지 반복해서 다시 생성
+            while "[" in generated_text:
+                y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
+                generated_text = decode(y[0].tolist())
+                generated_text = generated_text.replace("[", f"[{sample_count}]")
+
+            # 생성된 텍스트 출력
+            print(generated_text)
+            print("---------------")
+
+            # 생성된 텍스트를 파일로 저장
+            output_dir = "generated_samples"
+            os.makedirs(output_dir, exist_ok=True)
+            output_path = os.path.join(output_dir, f"sample_{sample_count}.txt")
             with open(output_path, "w") as file:
                 file.write(generated_text)
+
+            # 줄 수 증가 및 샘플 카운트 증가
+            total_lines += generated_text.count('\n') + 1  # 줄 바꿈 문자('\n')을 기준으로 줄 수 계산
+            sample_count += 1
